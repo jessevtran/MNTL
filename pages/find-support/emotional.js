db99656ebae57cebbe8678387f4466f309c02448
Jesse Tran <jtran58@gmu.edu>
2021-10-21 22:45:20 -0400

    Added

---
+++ pages/find-support/emotional.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+
+export default function Emotional() {
+  return (
+    <>
+      <IndexNavbar fixed />
+
+      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+        <div className="container mx-auto items-center flex flex-wrap">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+            <div className="pt-32 sm:pt-0">
+              <h2 className="font-semibold text-4xl text-blueGray-600">
+                Emotional
+              </h2>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
+          src="/img/pattern_nextjs.png"
+          alt="..."
+        />
+      </section>
+    </>
+  );
+}
