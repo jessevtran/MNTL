90f5e6207e7e4716db5881724334bee6dcc61156
sfekrat <90580332+sfekrat@users.noreply.github.com>
2021-11-02 17:17:45 -0400

    made changes in crisis page

--- pages/crisis/crisis.js
+++ pages/crisis/crisis.js
@@ -1,6 +1,8 @@
 import React from "react";
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import Head from "next/head";
+import Link from "next/link";
+import Footer from "components/Footers/Footer.js";
 
 const crisis = () => {
   return (
@@ -14,58 +16,111 @@ const crisis = () => {
           color: red;
           font-size: x-large;
         }
+        .cardBox {
+          margin-left: auto;
+          margin-right: auto;
+          display: flex;
+          flex-direction: column;
+        }
+
+        .card {
+          /* Add shadows to create the "card" effect */
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+          transition: 0.3s;
+          width: 800px;
+          height: 200px;
+          margin-bottom: auto;
+          margin-left: 150px;
+        }
+        h4 {
+          text-align: center;
+        }
       `}</style>
       <Head>
         <title>Crisis</title>
       </Head>
-      <IndexNavbar fixed/>
-      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
-        <div className="container mx-auto items-center flex flex-wrap">
-          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
-            <div className="pt-32 sm:pt-0">
-              <h2 className="font-semibold text-3xl text-blueGray-600 pt-6">
-                <li>
-                  In Case of Emergency: Dial <u style={{ color: "red" }}>911</u>
-                </li>
-              </h2>
-              <div>
-                <h2 className="font-semibold text-3xl text-blueGray-600 pt-12">
-                  <li>National Suicide Prevention Lifeline:</li>
-                </h2>
-                <p>Hours: Available 24 hours. Languages: English, Spanish.</p>
-                <p className="paragraphStyle">
-                  <strong>800-273-8255</strong>
-                </p>
-              </div>
-              <div>
-                <h2 className="font-semibold text-3xl text-blueGray-600 pt-12">
-                  <li>In a Crisis?</li>
-                </h2>
-                <p>Crisis Text Hotline:</p>
-                <p className="paragraphStyle">
-                  <strong>Text HOME to 741741</strong>
-                </p>
-              </div>
-              <div>
-                <h2 className="font-semibold text-3xl text-blueGray-600 pt-12">
-                  <li>Assault Helpline:</li>
-                </h2>
-                <p>Sexual Assault Hotline:</p>
-                <p className="paragraphStyle">
-                  <strong>800.656.4673</strong>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      <IndexNavbar fixed />
+      <section>
         <img
           className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
           src="/img/pattern_nextjs.png"
           alt="..."
         />
       </section>
+      <section className="cardBox">
+        <div className="card" style={{ margin: "170px 0 100px 150px" }}>
+          <div className="relative flex flex-row min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-blueGray-700">
+            <img
+              alt="..."
+              src="https://media.istockphoto.com/vectors/emergency-call-call-phone-in-hand-vector-illustration-vector-id688494412?k=20&m=688494412&s=612x612&w=0&h=zk6OfPoEbZRHpdIebIRVKww-AC2qsO8IIFtzjLlrMBs="
+              className="w-full align-middle rounded-t-lg"
+              style={{ height: "200px", width: "300px" }}
+            />
+            <blockquote className="relative p-4 mb-3">
+              <h4 className="text-l font-bold text-white">
+                In Cases of Emergency Dial 911
+              </h4>
+            </blockquote>
+          </div>
+        </div>
+        <div className="card" style={{ margin: "0 0 100px 150px" }}>
+          <div className="relative flex flex-row min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-blueGray-700">
+            <Link href="https://suicidepreventionlifeline.org/">
+              <a>
+                <img
+                  alt="..."
+                  src="https://suicidepreventionlifeline.org/wp-content/uploads/2016/08/NSPL_Logo.jpg"
+                  className="w-full align-middle rounded-t-lg"
+                  style={{ height: "200px", width: "300px" }}
+                />
+              </a>
+            </Link>
+            <blockquote className="relative p-4">
+              <h4 className="text-l font-bold text-white">
+                Available 24 hours
+              </h4>
+            </blockquote>
+          </div>
+        </div>
+        <div className="card">
+          <div className="relative flex flex-row min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-blueGray-700">
+            <Link href="https://www.rainn.org/">
+              <a>
+                <img
+                  alt="..."
+                  src="https://pbs.twimg.com/media/EPAnqiVWkAIXCoN.jpg"
+                  className="w-full align-middle rounded-t-lg"
+                  style={{ height: "200px", width: "300px" }}
+                />
+              </a>
+            </Link>
+            <blockquote className="relative p-4">
+              <h4 className="text-xl font-bold text-white">
+                Healing and Recovery Starts Here
+              </h4>
+            </blockquote>
+          </div>
+        </div>
+        <div className="card" style={{ margin: "100px 0 100px 150px" }}>
+          <div className="relative flex flex-row min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-blueGray-700">
+            <Link href="https://www.crisistextline.org/">
+              <a>
+                <img
+                  alt="..."
+                  src="https://images.squarespace-cdn.com/content/v1/5c0c00c1e74940cdaa7af6a1/1550687226454-QF2ZB5MGBX5NVP7SSDCT/Crisis+text+line"
+                  className="w-full align-middle rounded-t-lg"
+                  style={{ height: "200px", width: "300px" }}
+                />
+              </a>
+            </Link>
+            <blockquote className="relative p-4">
+              <h4 className="text-xl font-bold text-white">In Crisis?</h4>
+            </blockquote>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </>
   );
 };
-
 export default crisis;
