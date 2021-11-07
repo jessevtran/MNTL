92a4d0ec9042cebaa91eec54af9e228161d2eded
Jesse Tran <jtran58@gmu.edu>
2021-11-07 15:10:20 -0500

    Career wellness page

--- pages/find-support/career.js
+++ pages/find-support/career.js
@@ -9,35 +9,54 @@ export default function Career() {
     <>
       <IndexNavbar fixed />
 
-      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+      {/* <section className="header relative pt-16 items-center flex h-screen max-h-860-px"> */}
+      <section className="relative pt-48 items-center flex h-screen max-h-860-px mb-48">
         <div className="container mx-auto items-center flex flex-wrap">
-          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-12">
             <div className="pt-32 sm:pt-0">
-
-              <h2 className="font-semibold text-4xl text-blueGray-600">
-                Career
+              <h2 className="font-semibold text-4xl text-blueGray-600 mb-8">
+                Career Wellness
               </h2>
-              <div className="pt-6">
-                <p className="text-base font-light leading-relaxed mt-0 mb-4 text-blueGray-800">
-                  Career is an important aspect when it comes to an individual's prowess.
-                  Productivity and efficiency can benefit oneself by using these self-help resources
-                  listed below.
-
-                </p>
-              </div>
-              <h3 className="font-semibold text-2xl text-blueGray-600">
-                Tips
-              </h3>
-              <div className="pt-6">
-                <ul class="list-disc">
-                  <li> 1 </li>
-                  <li> 2 </li>
-                  <li> 3 </li>
-                  <li> 4 </li>
-                </ul>
-
-              </div>
             </div>
+
+            <h3 className="font-semibold text-2xl text-blueGray-600 mb-0">
+              What is Career Wellness?
+            </h3>
+
+            <p className="text-base font-light leading-relaxed mt-0 mb-6 text-blueGray-800 pt-0">
+              Career wellness is an important aspect when it comes to an individual's sense purpose and productivity.
+              It involves being able to balance school work, jobs, and activities outside of work, also known as work-life balance.
+              It is important to consider that the career that you want to work in fits your skills and interests, and makes you feel
+              good about what you've accomplished. However, you should also make sure to put yourself first; your mental, emotional, and physical
+              wellbeing is most important.
+            </p>
+
+            <h3 className="font-semibold text-2xl text-blueGray-600 mt-4">
+              Tips For Improving Career Wellness
+            </h3>
+
+            <div className="mb-6 pl-5 font-light text-blueGray-800">
+              <ul className="list-disc list-outside">
+                <li>Ask yourself: are you thinking about a major that you find interesting and meaningful?</li>
+                <li>Create goals for yourself. What do you want to achieve in life? </li>
+                <li>Don't settle. Keep motivated and keep working towards your goals.</li>
+                <li>As a student, take advantage of the resources around you! Ask professors for insight and professional advice! Sign up for career fairs and workshops</li>
+                <li>If your current job is stressful. Not enough personal time? Large tasks? reflect on your work-life balance and create new goals.</li>
+              </ul>
+            </div>
+            <div className="font-semibold text-2xl text-blueGray-600 mb-0">
+              <h3>
+                Resources TODO: BOXES FOR LIST ENTRIES
+              </h3>
+            </div>
+            <div className="mb-6 pl-5 font-light text-blueGray-800">
+            <ul className="list-disc list-outside">
+              <li>Check out <a href="https://gmu.joinhandshake.com/login" className="text-lightBlue-300 underline">Handshake</a>, Mason's source for jobs, internships, and career fairs.</li>
+              <li><a href="https://masononline.gmu.edu/university-career-services/" className="text-lightBlue-300 underline">University Career Services</a></li>
+              <li><a href="https://introwellness.com/health/what-is-occupational-wellness/" className="text-lightBlue-300 underline">https://introwellness.com/health/what-is-occupational-wellness/</a></li>
+            </ul>
+            </div>
+
           </div>
         </div>
         <img
