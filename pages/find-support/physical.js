ceb13c5b70c7b7ff064062a6c5fc69b48bc2eb11
Jesse Tran <jtran58@gmu.edu>
2021-11-07 16:01:02 -0500

    physical page

--- pages/find-support/physical.js
+++ pages/find-support/physical.js
@@ -9,38 +9,57 @@ export default function Physical() {
     <>
       <IndexNavbar fixed />
 
-      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+      <section className="relative pt-48 items-center flex h-screen max-h-860-px mb-48">
         <div className="container mx-auto items-center flex flex-wrap">
-
-          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-12">
             <div className="pt-32 sm:pt-0">
-
-              <h2 className="font-semibold text-4xl text-blueGray-600">
+              <h2 className="font-semibold text-4xl text-blueGray-600 mb-8">
                 Physical Wellness
               </h2>
-
-              <div className="pt-6">
-                <p>
-                  Physical Wellness plays an important role in your overall well-being and success.
-                  Not only does regular activity prevent diseases such as diabetes, heart disease,
-                  and high blood pressure, it is essential for reducing stress and the affects of
-                  depression and anxiety <a
-                    href="https://www.tandfonline.com/doi/full/10.1080/08039480802315640" >[1]
-                  </a>.
-                </p>
-              </div>
-
-              <div className="pt-6">
-                <p>
-
-                </p>
-
-
-              </div>
             </div>
+
+            <h3 className="font-semibold text-2xl text-blueGray-600 mb-0">
+              What is Physical Wellness?
+            </h3>
+
+            <p className="text-base font-light leading-relaxed mt-0 mb-6 text-blueGray-800 pt-0">
+              Physical Wellness plays an important role in your overall well-being and success.
+              Not only does regular activity prevent diseases such as diabetes, heart disease,
+              and high blood pressure, it is essential for reducing stress and the affects of
+              depression and anxiety <a
+                href="https://www.tandfonline.com/doi/full/10.1080/08039480802315640" className="text-lightBlue-300 underline">[1]
+              </a>.
+            </p>
+
+            <h3 className="font-semibold text-2xl text-blueGray-600 mt-4">
+              Tips For Improving Physical Wellness
+            </h3>
+
+            <div className="mb-6 pl-5 font-light text-blueGray-800">
+              <ul className="list-disc list-outside">
+                <li>Aim for at least 150 minutes of physical activity each week!</li>
+                <li>Try to get at least eight hours of sleep per night.</li>
+                <li>Stay hydrated! Drink plenty of water throughout the day and avoid soda or beverages high in sugar.</li>
+                <li>You don't have to be an olympic athlete to be physically well! Do activities that you enjoy, and make them part of your daily routine.</li>
+                <li>Track your progress. Start small and build up i.e. If you're walking, slowly increase walking time/distance.</li>
+              </ul>
+            </div>
+
+            <div className="font-semibold text-2xl text-blueGray-600 mb-0">
+              <h3>
+                Resources TODO: BOXES FOR LIST ENTRIES
+              </h3>
+            </div>
+
+            <div className="mb-6 pl-5 font-light text-blueGray-800">
+            <ul className="list-disc list-outside">
+              <li> <a href="https://www.nih.gov/health-information/physical-wellness-toolkit" className="text-lightBlue-300 underline">National Institute of Health: Physical Wellness Toolkit</a></li>
+              <li> <a href="https://shs.gmu.edu/healthed/exercise/" className="text-lightBlue underline">Mason Student Health Services: Exercise</a></li>
+              <li> <a href="https://www.heart.org/en/healthy-living/fitness/fitness-basics/why-is-physical-activity-so-important-for-health-and-wellbeing" className="text-lightblue-300 underline">Heart.org: Why is Physical Activity So Important For Health and Well-Being?</a></li>
+            </ul>
+            </div>
+
           </div>
-
-
         </div>
 
         <img
