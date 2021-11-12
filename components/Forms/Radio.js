91f99a51dde768709f615e06601aef8ec61aee45
Nnamdi Ojibe <chibizoar@gmail.com>
2021-11-12 10:58:42 -0500

    feature

--- components/Forms/Radio.js
+++ components/Forms/Radio.js
@@ -9,7 +9,7 @@ const Radio = ({name, value, title, props}) => {
               <Field type="radio" name={name} value={value} onChange={e => {props.name = parseInt(props.name) + parseInt(e.target.value) ; console.log(props.name) }} className="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" />
               {title ? title : value}
             </label>
-    
+    )
 }
 
 
