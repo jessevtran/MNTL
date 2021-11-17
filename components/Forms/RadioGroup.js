3dd2cf7e5f91173d53a6a6bfa7fed90d3d73644d
Nnamdi Ojibe <chibizoar@gmail.com>
2021-11-17 17:12:41 -0500

    Completed the wellness form input

--- components/Forms/RadioGroup.js
+++ components/Forms/RadioGroup.js
@@ -1,25 +1,58 @@
 import React from "react";
-import Radio from "./Radio";
+import { Field,} from "formik";
+
 
 /**
- *
- * @param {*} rangeOfAnswers # of possible radio choices.
- * @param {*} value type of question being asked (e.g. "Emotional", "Phsyical"). Used to map to Formik initalValues
+ * The question group for the problem at hand.
+ * @param {*} touched list of touched questions
+ * @param {*} errors list of questions that have been touched AND unanswered
+ * @param {*} name type of question being asked (e.g. "Emotional", "Phsyical"). Used to map to Formik initalValues
  * @param {*} question Question being asked.
  * @returns
  */
-export default function FormikRadioGroup({props,rangeOfAnswers, question, name}) {
-  const {values} = props
+export default function FormikRadioGroup({touched, errors, validateRadio, question, name}) {
+
+  /**
+   * Validation for the error
+   * @param {*} value current question in the form
+   * @returns error if the question is not completed
+   */
+  function validateRadio(value) {
+    let error;
+    if (!value) {
+      error = 'Required';
+    }
+    return error;
+  }
+
+  
   return (
-    <div class="relative py-4 px-4 flex items-center flex-col lg:flex-row  md:justify-between drop-shadow-sm rounded-md">
+    
+    <div class="relative py-4 px-4 flex items-center flex-col md:flex-row  md:justify-between drop-shadow-sm rounded-md">
+      <div class="flex flex-col py-2">
         <h6 class="text-xl font-bold text-center lg:text-left leading-normal">{question}</h6>
-      {/* START of LIST of RADIO OPTIONS*/}
-      <div className="relative flex flex-row px-4 space-x-4 justify-center">
-        {[...Array(rangeOfAnswers).keys()].map((num) => (
-          <Radio props={values} name={name} value={`${num++}`} />
-        ))}
+        {errors[name] && touched[name] && <p className="text-red-400">{errors[name]}</p>}
+        </div>
+      <div id='role' aria-labelledby="my-radio-group" className="relative flex flex-row px-4 space-x-4 justify-center">
+       {/**START of radio group answers */}
+        <p>Unlikely</p>   
+            <label>
+            <Field type="radio" name={name} value={'1'} validate={validateRadio}/>
+            </label>
+            <label>
+              <Field type="radio" name={name} value={'2'} validate={validateRadio}/>
+            </label>
+            <label>
+              <Field type="radio" name={name} value={'3'} validate={validateRadio} />
+            </label>
+            <label>
+              <Field type="radio" name={name} value={'4'} validate={validateRadio} />
+            </label>
+            <p>Likely</p>
+            {/**END of radio group answers */}
       </div>
-      {/* END of LIST of RADIO OPTIONS*/}
+      
     </div>
+    
   );
 }
--- components/Forms/WellnessAssessment/WellnessAssessmentForm.js
+++ components/Forms/WellnessAssessment/WellnessAssessmentForm.js
@@ -1,115 +1,141 @@
-import { Field, Form, Formik, getIn } from 'formik';
-import * as Yup from 'yup';
-import React from 'react';
-import Radio from '../Radio';
-import { questionsDB } from "utils/db.js"
-import FormikRadioGroup from '../RadioGroup';
+import { Form, Formik} from "formik";
+import { React } from "react";
+import { questionsDB } from "utils/db.js";
+import FormikRadioGroup from "../RadioGroup";
 
-const UpdateResults = () => {
-  // Grab values and submitForm from context
-  const { values } = useFormikContext();
-  React.useEffect(() => {
-    // Submit the form imperatively as an effect as soon as form values.token are 6 digits long
-    if (values.token.length === 6) {
-      submitForm();
-    }
-  }, [values]);
-  return null;
-};
-
-//TODO: DEVELOP AND TEST THE FOLLOWING COMPONENTS: Questions,... 
+//TODO: DEVELOP AND TEST THE FOLLOWING COMPONENTS: Questions,...
 /**
  * Wellness Assessment Form Wrapper Componenent
- * 
- * Utilizes Formik and Yup Validation.
- * @param {*} param0 
+ *
+ * Utilizes Formik
+ * @param {*} 
  */
 const WellnessAssessmentForm = () => {
-
   
-
-
-
+  //initial values
   const initialValues = {
-    Emotional: 0,
-    Physical: 0,
-    Intellectual: 0,
-    Environmental: 0,
-    Occupational: 0,
-    Social: 0,
-    Spiritual: 0,
+    Emotional0: "",
+    Emotional1: "",
+    Emotional2: "",
+    Emotional3: "",
+    Emotional4: "",
+    Emotional5: "",
+    Emotional6: "",
+    Environmental0: "",
+    Environmental1: "",
+    Environmental2: "",
+    Environmental3: "",
+    Environmental4: "",
+    Environmental5: "",
+    Environmental6: "",
+    Intellectual0: "",
+    Intellectual1: "",
+    Intellectual2: "",
+    Intellectual3: "",
+    Intellectual4: "",
+    Intellectual5: "",
+    Intellectual6: "",
+    Physical0: "",
+    Physical1: "",
+    Physical2: "",
+    Physical3: "",
+    Physical4: "",
+    Physical5: "",
+    Physical6: "",
+    Occupational0: "",
+    Occupational1: "",
+    Occupational2: "",
+    Occupational3: "",
+    Occupational4: "",
+    Occupational5: "",
+    Occupational6: "",
+    Social0: "",
+    Social1: "",
+    Social2: "",
+    Social3: "",
+    Social4: "",
+    Social5: "",
+    Social6: "",
+    Spiritual0: "",
+    Spiritual1: "",
+    Spiritual2: "",
+    Spiritual3: "",
+    Spiritual4: "",
+    Spiritual5: "",
+    Spiritual6: "",
   };
 
-  
-
-    //Validation Schemas for questions
-    const RADIO_GROUP_VALIDATION_SCHEMA = Yup.string().required('Select an option')
-
-    // Validation Schema for form
-    const ValidationSchema = Yup.object().shape({
-    radioGroup: RADIO_GROUP_VALIDATION_SCHEMA
-  });
-
-
-
-return (
+  return (
     <Formik
-    //Initial Values of questions
+
+      /**
+       * loads initial values from above
+       */
       initialValues={initialValues}
-    //submit Handler for form
-      onSubmit={submitHandler}
-    //valudationSchema for the form
-      validationSchema={ValidationSchema}
+
+      /**
+       * Tallies up the scores
+       */
+      onSubmit={async (initialValues) => {
+        await new Promise((r) => setTimeout(r, 500));
+
+        //Get the first Key, loop through rest until new 0 found. Repeat until done.
+        let results = {
+          Emotional: 0,
+          Environmental: 0,
+          Intellectual: 0,
+          Physical: 0,
+          Occupational: 0,
+          Social: 0,
+          Spiritual: 0,
+        };
+        
+        console.log(initialValues);
+       
+        var keys = Object.keys(initialValues)
+        console.log(keys)
+       
+        //tally up the scores
+        for (var i = 0; i < keys.length; i++) {
+          let value = initialValues[keys[i]]  
+          let accessKey = keys[i].replace(/\d+/g, '')
+          results[accessKey] += parseInt(value)
+        }
+
+        //FIXME: Temporary answer
+        alert(JSON.stringify(results))
+
+    
+        
+      }}
     >
-      {({values}) => (
-      <Form>
-        <div class="relative flex flex-col divide-y divide-solid">
-      {questionsDB.sections.map(section => (
-        section.questions.map(question =>
-        <FormikRadioGroup
-        props = {values}
-        question={question}
-        rangeOfAnswers={5}
-        name={section.type}
-        />
-        ))
-      )}
-      </div>
-      <div className="flex justify-center align-center">
-        <SubmissionButton/>
-      </div>
-      
-      </Form>
+      {({ values, errors, touched }) => (
+        <Form>
+          <div class="relative flex flex-col divide-y divide-solid">
+            {questionsDB.sections.map((section) =>
+              section.questions.map((question, index) => (
+                <FormikRadioGroup
+                  touched={touched}
+                  errors={errors}
+                  values={values}
+                  question={question}
+                  name={section.type + `${index++}`}
+                />
+              ))
+            )}
+          </div>
+          <div className="flex justify-center align-center">
+            <button
+              type="submit"
+              className="bg-blueGray-500 text-white active:bg-blueGray-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            >
+              Submit
+            </button>
+          </div>
+        </Form>
       )}
     </Formik>
-
-)
+  );
 };
 
-//TODO: SUBMIT HANDLER
-function submitHandler(){
-  async (values) => {
-    await new Promise((r) => setTimeout(r, 500));
-    alert(JSON.stringify(values, null, 2));
-  }
-}
-
-
-//TODO: ONCHANGE HANDLER
-const handleChange = event => {
-
-  
- }
-
-
-const SubmissionButton = () => {
-  return (
-    <button type="submit" className="bg-blueGray-500 text-white active:bg-blueGray-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-    Submit
-  </button>
-  )
-}
-
-
-export default WellnessAssessmentForm
-
+export default WellnessAssessmentForm;
