104ac8e397fa06e8215d936356af8261b5b66637
Jesse Tran <jtran58@gmu.edu>
2022-09-21 04:56:11 -0400

    misc cleanup

--- pages/wellness-quiz.js
+++ pages/wellness-quiz.js
@@ -1,13 +1,14 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from "react";
-import Link from "next/link";
 import WellnessForm from "components/Forms/WellnessAssessment/WellnessAssessmentForm.js";
+import { connect, getIn } from 'formik';
 
 import Head from "next/head";
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footers/Footer.js";
 
 export default function WellnessQuiz() {
+
   return (
     <>
       <Head>
@@ -17,15 +18,14 @@ export default function WellnessQuiz() {
       <div className="header relative pt-16 items-center flex ">
         <div className="container mx-auto justify-center flex">
           <div className="w-full px-4 my-4">
-            {/* //FIXME: Formink */}
+
             <div>
               <h2 className="font-semibold text-6xl text-blueGray-600 pt-20 pl-4">
                 Wellness Quiz
               </h2>
             </div>
 
-            {/* <div id="react-paginate"></div> <WellnessAssessmentForm /> */}
-            <WellnessForm/>
+            <WellnessForm />
 
           </div>
         </div>
@@ -35,7 +35,6 @@ export default function WellnessQuiz() {
       <div className="py-8" />
 
       <Footer />
-      <script type="text/javascript" src="components/Forms/WellnessAssessment/WellnessAssessmentForm.js"></script>
     </>
   );
 }
